7a96622be1a70c91095c9816e0f12623b04f8666
winton <mail@wintoni.us>
2020-06-19 00:19:22 -0700

    More httpRequester specs

--- src/test/httpRequesterSpec.ts
+++ src/test/httpRequesterSpec.ts
@@ -51,6 +51,39 @@ describe("httpRequester", () => {
     })
   })
 
+  it("POST / JSON", async () => {
+    const headers = {
+      "content-type": "application/json",
+    }
+
+    const httpIncomingMessage = new FakeIncomingMessage(
+      "POST",
+      "/",
+      headers
+    )
+
+    const promise = httpRequester.respond({
+      httpIncomingMessage,
+    })
+
+    httpIncomingMessage.emit(
+      "data",
+      '{ "foo": "bar", "baz": "bla" }'
+    )
+    httpIncomingMessage.emit("end")
+
+    expect(await promise).toEqual({
+      files: {},
+      headers,
+      method: "POST",
+      params: {
+        baz: "bla",
+        foo: "bar",
+      },
+      path: "/",
+    })
+  })
+
   it("POST / urlencoded", async () => {
     const headers = {
       "content-type":
@@ -77,6 +110,74 @@ describe("httpRequester", () => {
       params: {
         baz: "bla",
         foo: "bar",
+      },
+      path: "/",
+    })
+  })
+
+  it("POST / multipart", async () => {
+    const headers = {
+      "content-type":
+        "multipart/form-data; boundary=" +
+        "---------------------------paZqsnEHRufoShdX6fh0lUhXBP4k",
+    }
+
+    const httpIncomingMessage = new FakeIncomingMessage(
+      "POST",
+      "/",
+      headers
+    )
+
+    const promise = httpRequester.respond({
+      httpIncomingMessage,
+    })
+
+    httpIncomingMessage.emit(
+      "data",
+      [
+        "-----------------------------paZqsnEHRufoShdX6fh0lUhXBP4k",
+        'Content-Disposition: form-data; name="fileName0"',
+        "",
+        "super a file",
+        "-----------------------------paZqsnEHRufoShdX6fh0lUhXBP4k",
+        'Content-Disposition: form-data; name="fileName1"',
+        "",
+        "super b file",
+        "-----------------------------paZqsnEHRufoShdX6fh0lUhXBP4k",
+        'Content-Disposition: form-data; name="uploadFile0"; filename="1k_a.dat"',
+        "Content-Type: application/octet-stream",
+        "",
+        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+        "-----------------------------paZqsnEHRufoShdX6fh0lUhXBP4k",
+        'Content-Disposition: form-data; name="uploadFile1"; filename="1k_b.dat"',
+        "Content-Type: application/octet-stream",
+        "",
+        "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+        "-----------------------------paZqsnEHRufoShdX6fh0lUhXBP4k--",
+      ].join("\r\n")
+    )
+    httpIncomingMessage.emit("end")
+
+    expect(await promise).toEqual({
+      files: {
+        uploadFile0: {
+          encoding: "7bit",
+          mimetype: "application/octet-stream",
+          name: "1k_a.dat",
+          path: expect.any(String),
+        },
+        uploadFile1: {
+          encoding: "7bit",
+          mimetype: "application/octet-stream",
+          name: "1k_b.dat",
+          path: expect.any(String),
+        },
+      },
+      headers,
+      method: "POST",
+      params: {
+        fileName0: "super a file",
+        fileName1: "super b file",
       },
       path: "/",
     })
