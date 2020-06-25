8c8ae6613482cb74db21dd8ae1b0ca2e1c18fac7
winton <mail@wintoni.us>
2020-06-24 18:54:53 -0700

    Move http server code to task

--- src/tasks/startHttpTask.ts
+++ src/tasks/startHttpTask.ts
@@ -1,11 +1,21 @@
-import ptySpawner from "../lib/ptySpawner"
-import { join } from "path"
+import http from "http"
+import libHttpServer from "../lib/httpServer"
+import requester from "../lib/requester"
 
-export async function startHttpTask(): Promise<void> {
-  await ptySpawner("node", {
-    args: ["dist-cjs/servers/httpServer.js"],
-    cwd: join(__dirname, "../../"),
-    stdout: true,
+export async function startHttpTask(): Promise<
+  http.Server
+> {
+  const port = process.env.PORT
+    ? parseInt(process.env.PORT)
+    : 3000
+
+  return libHttpServer(port, async (incoming, response) => {
+    const result = await requester({
+      httpIncomingMessage: incoming,
+    })
+
+    response.write(JSON.stringify(result))
+    response.end()
   })
 }
 
