00e8b59a6bbe89fae2289a231b766044d35c6e96
winton <mail@wintoni.us>
2020-07-19 20:03:35 -0700

    exampleModel

---
+++ src/models/exampleModel.ts
@@ -0,0 +1,11 @@
+export interface ExampleModelInput {}
+
+export interface ExampleModelOutput {}
+
+export async function exampleModel(
+  input: ExampleModelInput
+): Promise<ExampleModelOutput> {
+  return {}
+}
+
+export default exampleModel
