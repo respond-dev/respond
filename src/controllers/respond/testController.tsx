99f7e0d28edf75a01aaeb65a34ef82ace96c96d9
winton <mail@wintoni.us>
2020-09-20 21:10:20 -0700

    JSON mode in test controller

--- src/controllers/respond/testController.tsx
+++ src/controllers/respond/testController.tsx
@@ -4,6 +4,10 @@ import { ControllerOutputType } from "types/controllerTypes"
 export async function testController(
   input: ControllerInputType
 ): Promise<ControllerOutputType> {
+  if (input.query.returnJson) {
+    return JSON.stringify({ path: input.url.path })
+  }
+
   return <div>{input.url.path}</div>
 }
 
