b9ff16a7477ec6b02412ec18a55cff08af2c339d
winton <mail@wintoni.us>
2020-09-29 18:29:44 -0700

    appRouter -> webRouter

--- src/apps/web/routers/webRouter.ts
+++ src/apps/web/routers/webRouter.ts
@@ -2,13 +2,12 @@ import routeSelector from "libs/respond/routeSelector"
 import { RouterInputType } from "types/respond/routerTypes"
 import { RouterOutputType } from "types/respond/routerTypes"
 
-export async function appRouter(
+export async function webRouter(
   input: RouterInputType
 ): Promise<RouterOutputType> {
   return await routeSelector("apps/web", input, [
     { matcher: "/", controller: "home", layout: "layout" },
-    { matcher: "/hi", controller: "hi", layout: "layout" },
   ])
 }
 
-export default appRouter
+export default webRouter
